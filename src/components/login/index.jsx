3dab2494fb54ac6e3ef9667de6ef0889559b2b4f
JakubKub13 <jakubkubala3@gmail.com>
2023-08-29 06:33:31 +0200

    debuggin

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,17 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import { usePostLoginMutation, usePostSignUpMutation } from "@/state/api";
- 
-function Login({ setUser, setSecret }) {
+
+const Login = ({ setUser, setSecret }) => {
   const [isRegister, setIsRegister] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [triggerLogin, resultLogin] = usePostLoginMutation();
   const [triggerSignUp] = usePostSignUpMutation();
 
-
   const handleLogin = () => {
     triggerLogin({ username, password });
-  }
+  };
 
   const handleRegister = () => {
     triggerSignUp({ username, password });
@@ -19,42 +18,45 @@ function Login({ setUser, setSecret }) {
 
   useEffect(() => {
     if (resultLogin.data?.response) {
-      setUsername(username);
-      setPassword(password);
+      setUser(username);
+      setSecret(password);
     }
   }, [resultLogin.data]); // eslint-disable-line
 
   return (
     <div className="login-page">
       <div className="login-container">
-        <h2 className="title">A.I. powered Chat app</h2>
-        <p className="register-change" onClick={() => setIsRegister(!isRegister)}>
-          {isRegister ? 'Already have an account?' : 'Create an account'}
+        <h2 className="title">CHATGPT APP</h2>
+        <p
+          className="register-change"
+          onClick={() => setIsRegister(!isRegister)}
+        >
+          {isRegister ? "Already a user?" : "Are you a new user?"}
         </p>
 
         <div>
-          <input 
-            className="login-input" 
-            type="text" 
-            placeholder="user" 
+          <input
+            className="login-input"
+            type="text"
+            placeholder="Username"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}  
+            onChange={(e) => setUsername(e.target.value)}
           />
-          <input 
-            className="login-input" 
-            type="secret" 
-            placeholder="secret" 
+          <input
+            className="login-input"
+            type="password"
+            placeholder="Password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}  
+            onChange={(e) => setPassword(e.target.value)}
           />
-          
         </div>
+
         <div className="login-actions">
           {isRegister ? (
             <button type="button" onClick={handleRegister}>
               Register
             </button>
-          ): (
+          ) : (
             <button type="button" onClick={handleLogin}>
               Login
             </button>
@@ -62,7 +64,7 @@ function Login({ setUser, setSecret }) {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
